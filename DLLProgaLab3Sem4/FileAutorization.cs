bc7bc5b41826546c52520d688050c8a111bc2498
user <user@CORE3>
2023-04-28 21:08:36 +0700

    Добавьте файлы проекта.

---
+++ DLLProgaLab3Sem4/FileAutorization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLL
+{
+    public class FileAutorization
+    {
+        string _filePath;
+
+        public FileAutorization(string filePath)
+        {
+            _filePath = filePath;
+        }
+        public bool Autorization(string userName, string Password, out string menuPath)
+        {
+            FileInfo f = new FileInfo(_filePath);
+            menuPath = "";
+
+            if (userName == "" || Password == "")
+            {
+                MessageBox.Show("Не введен логин или пароль");
+                return false;
+            }
+
+            using (StreamReader sr = new StreamReader(f.OpenRead(), Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] splitLine = line.Split(new char[] { ' ' });
+
+                    string name = splitLine[0];
+                    string password = splitLine[1];
+                    menuPath = splitLine[2];
+
+                    if (name == userName && password == Password)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        
+    }
+}
---
+++ DLLProgaLab3Sem4/MenuCommands.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLL
+{
+    public class MenuCommands
+    {
+        public void Others(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли разное");
+        }
+        public void Stuff(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли информацию о сотрудниках");
+        }
+        public void Orders(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли приказы");
+        }
+        public void Docs(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли документы");
+        }
+        public void Departs(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли отделы");
+        }
+        public void Towns(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли информацию о городах");
+        }
+        public void Posts(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли информацию о должниках");
+        }
+        public void Window(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли окна");
+        }
+        public void Content(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли оглавление");
+        }
+        public void About(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли информацию о программе");
+        }
+        public void Help(object sender, EventArgs e)
+        {
+            MessageBox.Show("Вы открыли помощь");
+        }      
+    }
+}
